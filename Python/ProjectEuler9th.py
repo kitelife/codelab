ce1aef432a3838597a0ed0823b4e11a596c54820
youngsterxyf <sas.198708@gmail.com>
2011-06-17 19:43:12 +0800

    modify the file ProjectEuler9th.py

--- Python/ProjectEuler9th.py
+++ Python/ProjectEuler9th.py
@@ -7,8 +7,8 @@ For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 '''
-for c in range(600,100,-1):
-	for a in range(100,500):
+for c in range(500,100,-1):
+	for a in range(0,333):
 		for b in range(100,500):
 			if a+b+c==1000 and a*a+b*b==c*c and a<b and b<c:
 				print "a=",a,";b=",b,";c=",c
