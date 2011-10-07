43870a504ff741c0100365e065aa3becbae5e039
unknown <Administrator@.(none)>
2011-10-07 16:30:26 +0800

    delete pickleSample/

--- Python/pickleSample/custom_class.py
+++
@@ -1,7 +0,0 @@
-class MyClass(object):
-	def __init__(self):
-		self.data=[]
-	def __str__(self):
-		return "Custom Class MyClass Data:: %s" % str(self.data)
-	def add_item(self,item):
-		self.data.append(item)
