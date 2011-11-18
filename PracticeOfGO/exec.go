2a1594bc3a17aeffa519f2f564acb44ad905fc92
xiayf <sas.198708@gmail.com>
2011-11-18 12:42:07 +0800

    add exec.go flag.go go.go

---
+++ PracticeOfGO/exec.go
@@ -0,0 +1,10 @@
+package main
+
+import "exec"
+import "fmt"
+
+func main() {
+	cmd := exec.Command("/bin/ls", "-l")
+	buf, err := cmd.Output()
+	fmt.Printf("%s\n",string(buf))
+}
---
+++ PracticeOfGO/flag.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	dnssec := flag.Bool("dnssec", false, "Request DNSSEC records")
+	port := flag.String("port", "53", "Set the query port")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] [name ...]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	fmt.Printf("dnssec: %v\n",*dnssec)
+	fmt.Printf("port: %v\n",*port)
+	flag.Usage()
+}
