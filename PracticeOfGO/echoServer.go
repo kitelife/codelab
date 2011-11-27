62fc83a9f5e1033a9acc8564e7cc9bb764223fad
xiayf <sas.198708@gmail.com>
2011-11-27 22:23:24 +0800

    add echoServer.go

---
+++ PracticeOfGO/echoServer.go
@@ -0,0 +1,33 @@
+package main
+import(
+	"net"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+
+	l, err := net.Listen("tcp","127.0.0.1:8053")
+	if err != nil {
+		fmt.Printf("Failure to listen: %s\n",err.String())
+	}
+	for {
+		//如果希望使用并发处理连接，可将Echo函数的调用方式改为goroutine.
+		if c, err := l.Accept(); err == nil {Echo(c)}
+	}
+}
+
+func Echo(c net.Conn) {
+
+	defer c.Close()
+	line, err := bufio.NewReader(c).ReadString('\n')
+	if err != nil {
+		fmt.Printf("Failure to read: %s\n", err.String())
+		return
+	}
+	_, err = c.Write([]byte(line))
+	if err != nil {
+		fmt.Printf("Failure to write: %s\n", err.String())
+		return
+	}
+}
