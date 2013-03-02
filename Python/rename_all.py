b994a85821ee060b070e0de12c904a8081050d1d
youngsterxyf <sas.198708@gmail.com>
2013-03-02 23:47:27 +0800

    update rename_all.py

--- Python/rename_all.py
+++ Python/rename_all.py
@@ -8,19 +8,15 @@ def rename_files(dir_name):
         dir_name += '/'
 
     file_names = os.listdir(dir_name)
-    try:
-        file_names.remove('.')
-        file_names.remove('..')
-    except Exception:
-        pass
     name_template = '%s.%s'
     count = 1
     for filename in file_names:
-        file_type = filename.split('.')[-1]
-        new_name = dir_name + name_template %(str(count), file_type)
-        print filename, '--->', new_name
-        os.rename(dir_name + filename, new_name)
-        count +=1 
+        if not filename.startswith('.'):
+            file_type = filename.split('.')[-1]
+            new_name = dir_name + name_template %(str(count), file_type)
+            print filename, '--->', new_name
+            os.rename(dir_name + filename, new_name)
+            count +=1
 
 if __name__ == '__main__':
     arg_len = len(sys.argv)
