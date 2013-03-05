d8d57cb99b2b8e6b6986d12a915c56cc0027f364
youngsterxyf <sas.198708@gmail.com>
2013-03-06 03:44:54 +0800

    delete PHPBookmark and miniblog

--- PHP/miniBlog/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title="Hacker's Whiteboard";
-
-$dbHost="localhost";
-$dbUser="root";
-$dbPasswd="";
-$dbName="miniblog";
-?>
--- PHP/miniBlog/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('config.php');
-require_once('view_functions.php');
-do_html_header($title);
-display_login_form();
-do_html_footer();
-?>
