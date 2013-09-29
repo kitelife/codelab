fab967c1e8d2bfa97ad21778b5f697ae2c0e0dfe
yongfengxia <yongfengxia@tencent.com>
2013-09-29 11:22:01 +0800

    add new golang code file

---
+++ GoLang/fmtPractice.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var foo string = "This is a simple string"
+	fmt.Printf("%v\n", foo)
+	fmt.Printf("%T\n", foo)
+	fmt.Printf("%#v\n", foo)
+
+	type InnerNode struct {
+		X string
+	}
+
+	type Node struct {
+		A string
+		B int
+		C InnerNode
+	}
+	c := InnerNode{
+		X: "Hello",
+	}
+	n := Node{
+		A: "world",
+		B: 100,
+		C: c,
+	}
+
+	fmt.Printf("%+v\n", n)
+	fmt.Printf("%v\n", n)
+	fmt.Printf("%#v\n", n)
+	fmt.Printf("%T\n", n)
+}
