1342a1eb9d1754683cf9993ffb328e07c28146b9
unknown <xiayongfeng@XIAYONGFENG-PC.internal.baidu.com>
2014-05-15 11:16:50 +0800

    update

--- Python/fake_weixin.py
+++ Python/fake_weixin.py
@@ -32,12 +32,25 @@ class Weixin:
 			'Referer': 'https://mp.weixin.qq.com/',
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
 		}
-		r = self.session.post(self.login_url, data=form_data, headers=additional_headers)
-		if r.status_code == 200:
-			data = r.json()
-			redirect_url = data['redirect_url']
-			self.token = redirect_url.split('=')[-1]
-			print self.token
+		try_times = 0
+		is_succeed = False
+		while (not is_succeed) and try_times < 4:
+			try:
+				r = self.session.post(self.login_url, data=form_data, headers=additional_headers)
+				if r.status_code == 200:
+					data = r.json()
+					if data['base_resp']['ret'] != 0 or data['base_resp']['err_msg'] != 'ok':
+						print u'登录失败，请检查用户名密码是否争取！'
+						return
+					redirect_url = data['redirect_url']
+					self.token = redirect_url.split('=')[-1]
+					print self.token
+				else:
+					print u'未能成功登录，可能是登录链接有问题！'
+				is_succeed = True
+			except Exception as e:
+				print e.message
+			try_times += 1
 
 	def parse_users(self):
 		target_url = 'https://mp.weixin.qq.com/cgi-bin/contactmanage?t=user/index&pagesize=10&pageidx=0&type=0&token=%s&lang=zh_CN' %(self.token, )
@@ -45,16 +58,25 @@ class Weixin:
 			'Referer': 'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=%s' %(self.token, ),
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
 		}
-		r = self.session.get(target_url, headers=additional_headers)
-		users = None
-		if r.status_code == 200:
-			result = re.findall('friendsList\s*:\s*\((.+)\)\.contacts', r.content)
-			if len(result):
-				contacts = json.loads(result[0])
-				users = contacts['contacts']
-		if users:
-			self.user_list = users
-		print self.user_list
+		is_succeed = False
+		try_times = 0
+		while (not is_succeed) and try_times < 4:
+			try:
+				r = self.session.get(target_url, headers=additional_headers)
+				users = None
+				if r.status_code == 200:
+					is_succeed = True
+					result = re.findall('friendsList\s*:\s*\((.+)\)\.contacts', r.content)
+					if len(result):
+						contacts = json.loads(result[0])
+						users = contacts['contacts']
+				else:
+					self.fake_login()
+				if users:
+					self.user_list = users
+			except Exception as e:
+				print e.message
+			try_times += 1
 
 
 	def single_send(self, msg):
@@ -78,10 +100,23 @@ class Weixin:
 				'ajax': '1',
 				't': 'ajax-response'
 			}
-			r = self.session.post(target_url, data=form_data, headers=additional_headers)
-			if r.status_code == 200:
-				print r.text
-			sleep(2)
+			is_succeed = False
+			try_times = 0
+			while (not is_succeed) and try_times < 4:
+				try:
+					r = self.session.post(target_url, data=form_data, headers=additional_headers)
+					if r.status_code == 200:
+						data = r.json()
+						print data
+						if data['base_resp']['ret'] != 0 or data['base_resp']['err_msg'] != 'ok':
+							print u'向 %s 发送消息失败，%s' %(user['nick_name'], data['base_resp']['err_msg'])
+						is_succeed = True
+					else:
+						self.fake_login()
+				except Exception as e:
+					print u'访问异常，%s' %(e.message, )
+				try_times += 1
+				sleep(1)
 
 
 if __name__ == '__main__':
