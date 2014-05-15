855da9c7b06fb9b6d66f68e9e9525997a62043b9
夏永锋 <youngsterxyf@users.noreply.github.com>
2014-05-15 10:37:03 +0800

    Update fake_weixin.py

--- Python/fake_weixin.py
+++ Python/fake_weixin.py
@@ -4,6 +4,7 @@ import hashlib
 import json
 import re
 import random
+from time import sleep
 
 import requests
 
@@ -16,6 +17,7 @@ class Weixin:
 		self.md5_password = hashlib.md5(self.password).hexdigest()
 		self.session = requests.Session()
 		self.login_url = login_url
+		self.token = ''
 		self.user_list = []
 	
 	def fake_login(self):
@@ -31,13 +33,16 @@ class Weixin:
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
 		}
 		r = self.session.post(self.login_url, data=form_data, headers=additional_headers)
-		print r.status_code
-		print r.text
+		if r.status_code == 200:
+			data = r.json()
+			redirect_url = data['redirect_url']
+			self.token = redirect_url.split('=')[-1]
+			print self.token
 
 	def parse_users(self):
-		target_url = 'https://mp.weixin.qq.com/cgi-bin/contactmanage?t=user/index&pagesize=10&pageidx=0&type=0&token=1348857888&lang=zh_CN'
+		target_url = 'https://mp.weixin.qq.com/cgi-bin/contactmanage?t=user/index&pagesize=10&pageidx=0&type=0&token=%s&lang=zh_CN' %(self.token, )
 		additional_headers = {
-			'Referer': 'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=1348857888',
+			'Referer': 'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=%s' %(self.token, ),
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
 		}
 		r = self.session.get(target_url, headers=additional_headers)
@@ -49,6 +54,7 @@ class Weixin:
 				users = contacts['contacts']
 		if users:
 			self.user_list = users
+		print self.user_list
 
 
 	def single_send(self, msg):
@@ -56,7 +62,7 @@ class Weixin:
 		for user in self.user_list:
 			additional_headers = {
 				'Origin': 'https://mp.weixin.qq.com', 
-				'Referer': 'https://mp.weixin.qq.com/cgi-bin/singlesendpage?t=message/send&action=index&tofakeid=%s&token=1348857888&lang=zh_CN' %(user['id'], ),
+				'Referer': 'https://mp.weixin.qq.com/cgi-bin/singlesendpage?t=message/send&action=index&tofakeid=%s&token=%s&lang=zh_CN' %(user['id'], self.token),
 				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
 				'X-Requested-With': 'XMLHttpRequest'
 			}
@@ -65,7 +71,7 @@ class Weixin:
 				'content': msg,
 				'tofakeid': user['id'],
 				'imgcode': '',
-				'token': '1348857888',
+				'token': self.token,
 				'lang': 'zh_CN',
 				'random': random.random(),
 				'f': 'json',
@@ -75,10 +81,13 @@ class Weixin:
 			r = self.session.post(target_url, data=form_data, headers=additional_headers)
 			if r.status_code == 200:
 				print r.text
+			sleep(2)
 
 
 if __name__ == '__main__':
 	wx = Weixin('xxx', 'xxx', 'https://mp.weixin.qq.com/cgi-bin/login?lang=zh_CN')
 	wx.fake_login()
+	sleep(1)
 	wx.parse_users()
-	wx.single_send(u'Hello!')
+	sleep(1)
+	wx.single_send(u'Hello, world!')
