0da6e14967c61721ba4e546d9c7ee32fe37500fe
xiayongfeng <xiayongfeng@baidu.com>
2015-01-22 13:33:46 +0800

    new code

---
+++ GoLang/mkdir-permission.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "syscall"
+)
+
+func main() {
+    // 这两行必须！
+    mask := syscall.Umask(0)
+    defer syscall.Umask(mask)
+
+    err := os.MkdirAll("/tmp/gotest/", 0777)
+    if err != nil {
+      panic(err)
+    }
+
+    fmt.Println("Mkdir /tmp/gotest/")
+}
