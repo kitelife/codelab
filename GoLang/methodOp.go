8f928ff4733698597b3f633a208d886120af440a
youngsterxyf <sas.198708@gmail.com>
2016-09-16 12:00:15 +0800

    new go file

---
+++ GoLang/methodOp.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+根据调用者不同，方法分为两种表现形式：
+
+instance.method(args...) ---> <type>.func(instance, args...)
+
+前者称为 method value, 后者 method expression
+
+两者都可像普通函数那样赋值和传参，
+区别在于method value绑定实例，而method expression则须显式传参
+*/
+
+import (
+	"fmt"
+)
+
+type User struct {
+	id   int
+	name string
+}
+
+func (self *User) Test() {
+	fmt.Printf("%p, %v\n", self, self)
+}
+
+func main() {
+	u := User{1, "Tom"}
+	u.Test()
+
+	mValue := u.Test
+	mValue() // 隐式传递 receiver
+
+	mExpression := (*User).Test
+	mExpression(&u) // 显式传递 receiver
+}
