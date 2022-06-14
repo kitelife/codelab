f82186547e518bbb084c8344c2a324a812dbfc19
zhengqing <youngsterxyf@hotmail.com>
2022-06-14 17:58:06 +0800

    learning cuda programming

---
+++ CC++/cuda/hello4.cu
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+__global__ void hello_from_gpu()
+{
+  const int bid = blockIdx.x;
+  const int tid = threadIdx.x;
+  printf("Hello World from block %d and thread %d!\n", bid, tid);
+}
+
+int main(void)
+{
+  hello_from_gpu<<<2, 4>>>();
+  cudaDeviceSynchronize();
+  return 0;
+}
